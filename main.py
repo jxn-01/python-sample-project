43f3b5769a799447769d8a777251f8e60f5f6aca
jxn-01 <jeckson0@gmail.com>
2026-05-28 13:42:58 +0530

    Add greet and add_numbers functions in main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def greet(name: str) -> str:
+    return f"Hello, {name}! Welcome to my GitHub project."
+
+def add_numbers(a: int, b: int) -> int:
+    return a + b
+
+if __name__ == "__main__":
+    print(greet("Jeckson"))
+    print("2 + 3 =", add_numbers(2, 3))
